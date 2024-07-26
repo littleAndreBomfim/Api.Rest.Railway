d76068ec0b2ab29787200be3e0714457c12317e3
andre <strongerandrebomfim@gmail.com>
2024-07-26 14:50:27 -0300

    Initial commit

---
+++ Api.Rest.Railway/src/test/java/andre/Api/Rest/Railway/ApplicationTests.java
@@ -0,0 +1,13 @@
+package andre.Api.Rest.Railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

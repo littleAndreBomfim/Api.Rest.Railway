ed3e8b5991929103dda66c2ca3d41272aa8a4742
andre <strongerandrebomfim@gmail.com>
2024-07-26 15:29:10 -0300

    Adicionando configurações com o yml, e o model

---
+++ Api.Rest.Railway/src/main/java/andre/Api/Rest/Railway/domain/model/News.java
@@ -0,0 +1,8 @@
+package andre.Api.Rest.Railway.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
---
+++ Api.Rest.Railway/src/main/java/andre/Api/Rest/Railway/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package andre.Api.Rest.Railway.domain.repository;
+
+import andre.Api.Rest.Railway.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
